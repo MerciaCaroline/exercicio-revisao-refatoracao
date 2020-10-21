13b26b9dd5b141b045aef38fe5bab7694794d9da
Mércia Caroline <merciamartins07@yahoo.com.br>
2020-10-20 22:12:20 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -8,21 +8,25 @@ class Empregado {
 	
   public:
     double salarioHora;  
-    double quotaMensalVendas;  
+    double quotaMensalVendas;
+    static constexpr int CargaHoraria = 8;
+std::string NomeEmpregado;
 
 
     double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
+      double CargaCumprida = horasTrabalhadas;
 	  
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
+      if (horasTrabalhadas > CargaHoraria) {
+        double extra = horasTrabalhadas - 8;
+        CargaCumprida += extra / 2;
       }
-	  return t * salarioHora;
+	  return CargaCumprida * salarioHora;
     }
-	
+virtual void print(){
+      std::cout << "Nome: " << this->NomeEmpregado << std::endl;
+      std::cout << "Salario: " << this->pagamentoMes(this->horasTrabalhadas) << std::endl;
+    }
 };
 
 #endif
