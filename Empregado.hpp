a8e3a17f834371bb57c7b065dc4c14ca3abd9ce2
Mércia Caroline <merciamartins07@yahoo.com.br>
2020-10-20 23:17:09 -0300

    Update Empregado.hpp
    A superclasse Empregado carrega agora uma string de nome, e o número de carga horaria minima foi armazenado em uma constante para que nao tenha numeros misteriosos.
    A função que calcula o pagamento tinha variáveis com nome insignificativos que foram alterados.

--- Empregado.hpp
+++ Empregado.hpp
@@ -5,28 +5,25 @@
 #include <string>
 
 class Empregado {
-	
+
   public:
-    double salarioHora;  
-    double quotaMensalVendas;
+    double salarioHora;
+    double horasTrabalhadas;
     static constexpr int CargaHoraria = 8;
-std::string NomeEmpregado;
+    std::string NomeEmpregado;
 
 
     double pagamentoMes(double horasTrabalhadas) {
       double CargaCumprida = horasTrabalhadas;
-	  
+
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
       if (horasTrabalhadas > CargaHoraria) {
-        double extra = horasTrabalhadas - 8;
-        CargaCumprida += extra / 2;
+        double extra = horasTrabalhadas - CargaHoraria;
+        CargaCumprida += (extra / 2);
       }
 	  return CargaCumprida * salarioHora;
     }
-virtual void print(){
-      std::cout << "Nome: " << this->NomeEmpregado << std::endl;
-      std::cout << "Salario: " << this->pagamentoMes(this->horasTrabalhadas) << std::endl;
-    }
+
 };
 
 #endif
