917155647ff3be6e625dcaf0ec09bc8814dc45fb
Mércia Caroline <merciamartins07@yahoo.com.br>
2020-10-20 22:22:43 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,31 @@
 #include <string>
+#include <iostream>
 #include "Empregado.hpp"
 
 class Vendedor : public Empregado {
 
   public:
-    std::string nome;  	  
+	static constexpr int Meses_Trabalhados = 12;
+	double quotaMensalVendas;
+	
+	Vendedor(std::string nome, double salarioHora, double quotaMensalVendas, double horasTrabalhadas){
+      nome = nome;
+      salarioHora = salarioHora;
+      quotaMensalVendas = quotaMensalVendas;
+      horasTrabalhadas = horasTrabalhadas;
+    }
+	
 	
 	double quotaTotalAnual() {
 	  return quotaMensalVendas * 12;
 	}
+	
+	virtual void print(){
+      std::cout << "Nome: " << this->nome << std::endl;
+      std::cout << "Salario: " << this->pagamentoMes(this->horasTrabalhadas) << std::endl;
+      std::cout << "Quota vendas: " << this->quotaTotalAnual() << std::endl;
+      std::cout << std::endl;
+    }
 	
 };
 
