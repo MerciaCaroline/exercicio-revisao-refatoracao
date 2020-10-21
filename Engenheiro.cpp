5dc89b7a5dc26ed2c7ba2d3cc9fef24c824b06ec
Mércia Caroline <merciamartins07@yahoo.com.br>
2020-10-20 22:14:41 -0300

    Update Engenheiro.cpp

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,26 @@
 #include <string>
+#include <iostream>
+
 #include "Empregado.hpp"
 
 class Engenheiro : public Empregado {
 
   public:
-    std::string nome;  
 	int projetos;
 	
+	
+	Engenheiro(std::string nome, double salarioHora, int projetos, double horasTrabalhadas){
+      nome = nome;
+      salarioHora = salarioHora;
+      projetos = projetos;
+      horasTrabalhadas = horasTrabalhadas;
+    }
+
+    virtual void print(){
+      std::cout << "Nome: " << this->nome << std::endl;
+      std::cout << "Salario: " << this->pagamentoMes(this->horasTrabalhadas) << std::endl;
+      std::cout << "Projetos: " << this->projetos << std::endl;
+      std::cout << std::endl;
+    }
 };
 
